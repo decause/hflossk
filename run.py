ba5bcbe5b3ad795737aadd3c2aabd9444e058ef8
RemyD <remyd@civx.us>
2013-08-30 13:49:07 -0400

    Back to the "normal" way of checking blogs

--- run.py
+++ run.py
@@ -35,7 +35,6 @@ yaml_dir = 'scripts/people/'
 
 @app.route('/checkblogs')
 def checkblogs():
-
     try:
         urllib2.urlopen("http://foss.rit.edu", timeout=15)
     except:
@@ -103,6 +102,66 @@ def checkblogs():
                                gravatar=gravatar, average=average,
                                target_number=target_number)
 
+    #Raw block, no try/except
+    #student_data = []
+    #for fname in glob.glob(yaml_dir + "*.yaml"):
+    #    with open(fname) as students:
+    #        contents = yaml.load(students)
+
+    #        if not isinstance(contents, list):
+    #            raise ValueError("%r is broked" % fname)
+
+    #        student_data.extend(contents)
+    #        #print gravatar(contents[0]['rit_dce'] + "@rit.edu")
+
+    #student_posts = {}
+    #target = datetime(2013, 8, 26)
+    #for student in student_data:
+    #    when = []
+    #    if student.get('feed'):
+    #        print('Checking %s' % student['irc'])
+
+    #        feed = feedparser.parse(student['feed'])
+    #        #print feed.version
+
+    #        for item in feed.entries:
+    #            #from pprint import pprint
+    #            #pprint(item)
+    #            #if 'published' in item:
+    #            #    print student['name'], item.published
+    #            ##if 'summary' in item:
+    #            ##    print item.summary
+    #            publish_time = datetime.fromtimestamp(time.mktime
+    #                                                    (item.updated_parsed))
+    #            if publish_time < target:
+    #                #print('%s is older than %s, ignoring' % (publish_time,
+    #                #                                         target))
+    #                continue
+    #            when.append(item.updated)
+    #    else:
+    #        print('No feed listed for %s!' % student['irc'])
+
+    #    student_posts[student['irc']] = len(when)
+
+    #average = sum(student_posts.values()) / float(len(student_posts))
+    ##print('Average of %f posts' % average)
+    #target_number = (datetime.today() - target).total_seconds() /\
+    #    timedelta(weeks=1).total_seconds()
+    ##for student, count in student_posts.items():
+    ##    if count > target_number:
+    ##        print('+++%d %s' % (count, student))
+    ##    elif count < target_number:
+    ##        print('---%d %s' % (count, student))
+    ##    else:
+    ##        print('===%d %s' % (count, student))
+    ##for student in student_data:
+    ##    print student
+    #return render_template('blogs.mak', name='mako',
+    #                        student_data=student_data,
+    #                        student_posts=student_posts,
+    #                        gravatar=gravatar, average=average,
+    #                        target_number=target_number)
+
 
 @app.route('/')
 def index():
@@ -137,6 +196,7 @@ def about():
 @app.route('/hw/firstflight')
 def fflight():
     return render_template('fflight.mak', name='mako')
+
 
 @app.route('/books')
 def books():
