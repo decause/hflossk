51f74cfd8ac33cebc1118cc0d983421665900092
Nathaniel Case <Qalthos@gmail.com>
2013-11-20 13:25:20 -0500

    This is stale code, and I'm rewriting it anyway.

--- hflossk/run.py
+++ hflossk/run.py
@@ -137,66 +137,6 @@ def checkblogs():
                                student_teamproposals=student_teamproposals,
                                student_names=student_names)
 
-    #Raw block, no try/except
-    #student_data = []
-    #for fname in glob.glob(yaml_dir + "*.yaml"):
-    #    with open(fname) as students:
-    #        contents = yaml.load(students)
-
-    #        if not isinstance(contents, list):
-    #            raise ValueError("%r is broked" % fname)
-
-    #        student_data.extend(contents)
-    #        #print gravatar(contents[0]['rit_dce'] + "@rit.edu")
-
-    #student_posts = {}
-    #target = datetime(2013, 8, 26)
-    #for student in student_data:
-    #    when = []
-    #    if student.get('feed'):
-    #        print('Checking %s' % student['irc'])
-
-    #        feed = feedparser.parse(student['feed'])
-    #        #print feed.version
-
-    #        for item in feed.entries:
-    #            #from pprint import pprint
-    #            #pprint(item)
-    #            #if 'published' in item:
-    #            #    print student['name'], item.published
-    #            ##if 'summary' in item:
-    #            ##    print item.summary
-    #            publish_time = datetime.fromtimestamp(time.mktime
-    #                                                    (item.updated_parsed))
-    #            if publish_time < target:
-    #                #print('%s is older than %s, ignoring' % (publish_time,
-    #                #                                         target))
-    #                continue
-    #            when.append(item.updated)
-    #    else:
-    #        print('No feed listed for %s!' % student['irc'])
-
-    #    student_posts[student['irc']] = len(when)
-
-    #average = sum(student_posts.values()) / float(len(student_posts))
-    ##print('Average of %f posts' % average)
-    #target_number = (datetime.today() - target).total_seconds() /\
-    #    timedelta(weeks=1).total_seconds()
-    ##for student, count in student_posts.items():
-    ##    if count > target_number:
-    ##        print('+++%d %s' % (count, student))
-    ##    elif count < target_number:
-    ##        print('---%d %s' % (count, student))
-    ##    else:
-    ##        print('===%d %s' % (count, student))
-    ##for student in student_data:
-    ##    print student
-    #return render_template('blogs.mak', name='mako',
-    #                        student_data=student_data,
-    #                        student_posts=student_posts,
-    #                        gravatar=gravatar, average=average,
-    #                        target_number=target_number)
-
 
 @app.route('/oer')
 def oer():
