1a5ec012151084eee23e4a61c06b7e0f22ca7ad9
Samuel Lucidi <mansam@csh.rit.edu>
2014-02-13 21:05:12 -0500

    Fix brain damage.

--- app.py
+++ app.py
@@ -9,10 +9,9 @@ if __name__ == "__main__":
     if 'OPENSHIFT_PYTHON_IP' in os.environ:
         host = os.environ['OPENSHIFT_PYTHON_IP']
         port = int(os.environ['OPENSHIFT_PYTHON_PORT'])
-        app.run(host=host, port=port)
-	http_server = HTTPServer(WSGIContainer(app))
-	http_server.listen(port, address=host)
-	IOLoop.instance().start()
+        http_server = HTTPServer(WSGIContainer(app))
+        http_server.listen(port, address=host)
+        IOLoop.instance().start()
     else:
         app.debug = True
         app.run()
