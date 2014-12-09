8cebba6e994af1346fd2d394b091a8519cd7ef79
RemyD <remyd@civx.us>
2014-12-09 18:32:31 -0500

    Added quiz2 to the templates

--- hflossk/participants.py
+++ hflossk/participants.py
@@ -88,7 +88,7 @@ def participants(root_dir):
     assignments = ['quiz1', 'litreview1', 'bugfix', 'teamprop1', 'meetup1',
                    'commarchreport', 'commarchpreso', 'meetup2', 'curriculum',
                    'smoketest', 'vidreview1', 'vidreview2', 'teamprop2',
-                   'litreview2', 'meetup3']
+                   'litreview2', 'meetup3', 'quiz2']
     elapsed = (datetime.today() - hflossk.site.COURSE_START).total_seconds()
     target_number = int(elapsed / timedelta(weeks=1).total_seconds() + 1 +
                         len(assignments))
